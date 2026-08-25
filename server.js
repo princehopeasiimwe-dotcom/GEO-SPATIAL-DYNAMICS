86226216f21bc84f4c541b678e9a4e3f1ddde571
ASIIMWE HOPE <princehopeasiimwe@gmail.com>
2026-08-25 09:01:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`GDL site running at http://localhost:${PORT}`);
-  console.log(`Admin panel at http://localhost:${PORT}/admin`);
+  console.log(`GDL server running on port ${PORT}`);
+  console.log(`Admin panel at ${PORT}/admin`);
 });
